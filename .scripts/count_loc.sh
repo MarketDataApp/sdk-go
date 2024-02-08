fada8d10f16520122bba7d33d7c4d6dbe311bde7
MarketDataApp <github@cdnhq.net>
2024-02-08 14:18:31 -0300

    fix CI/CD issues

--- .scripts/count_loc.sh
+++ .scripts/count_loc.sh
@@ -2,9 +2,8 @@
 
 # Count lines of code excluding single-line comments, block comments, and blank lines in Go files.
 
-# Function to filter and count lines
 count_lines() {
-    local file=$1
+    local file="$1"
     # Change to the root directory of the git repository
     pushd "$(git rev-parse --show-toplevel)" > /dev/null
     # Remove single-line comments, block comments, and blank lines, then count
@@ -29,21 +28,24 @@ echo "Total lines of code (excluding comments and blank lines): $total"
 
 # Path to the README file
 README_PATH="$(git rev-parse --show-toplevel)/README.md"
+echo "README_PATH is set to: $README_PATH"
 
 # Determine OS and set sed command accordingly
 OS="$(uname)"
-SED_CMD=""
-
-if [ "$OS" == "Linux" ]; then
-    SED_CMD="sed -i"
-elif [ "$OS" == "Darwin" ]; then
-    SED_CMD="sed -i ''"
-else
-    echo "Unsupported OS"
-    exit 1
-fi
+echo "Operating System: $OS"
 
 # Update the LOC badge in the README.md
-$SED_CMD "s/lines_of_code-[0-9]\+/lines_of_code-${total}/" "$README_PATH"
+echo "Attempting to update LOC in README.md..."
+if [ "$OS" == "Darwin" ]; then
+    # Ensure the pattern matches exactly what's in your README.md, including any fixed parts of the badge URL
+    sed -i '' "s/lines_of_code-[0-9]\{1,\}/lines_of_code-${total}/" "$README_PATH" && echo "Update attempt made."
+else
+    echo "This script currently supports updates on Darwin (macOS) only."
+fi
 
-echo "Updated LOC in README.md to ${total}"
+# Verification step
+if grep -q "lines_of_code-${total}" "$README_PATH"; then
+    echo "Verification: README.md successfully updated."
+else
+    echo "Verification: Update to README.md failed or no change needed."
+fi
